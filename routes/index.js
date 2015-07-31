0f66e7d478dede2baa52ae8b041f52edfdcdb4e6
Judy Lin <judith.s.lin@gmail.com>
2015-07-30 19:41:46 -0600

    nav completed

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'BPOD' });
 });
 
+router.get('/parks', function(req, res, next) {
+  res.render('bpod/parks');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('bpod/about');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('bpod/contact');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('bpod/login');
+});
+
 module.exports = router;
