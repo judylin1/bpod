dd4c85529dfa9821710400e4791ae50d5ac28504
Judy Lin <judith.s.lin@gmail.com>
2015-08-06 19:43:54 -0600

    cookie test

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 var express = require('express');
 var path = require('path');
+require('dotenv').load();
+var unirest = require('unirest');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var monk = require('monk')(process.env.MONGOLAB_URI)
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
-require('dotenv').load();
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-// var auth = require('./routes/auth');
+var auth = require('./routes/auth');
+
+var session = require('cookie-session');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(session({
+  name: 'session',
+  keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]
+}))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -27,9 +36,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-// app.use('/auth', auth);
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.use('facebook', new FacebookStrategy({
   clientID        : process.env.FBID,
@@ -76,6 +84,10 @@ passport.use('facebook', new FacebookStrategy({
       });
     });
 }));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/auth', auth);
 
 var sendgrid_api_key = process.env.SENDGRIDKEY;
 var to = process.env.TO;
